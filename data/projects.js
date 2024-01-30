3580cc384738f344c434d10e4a0341331566c5ea
aly <aliatalla93@gmail.com>
2024-01-30 21:45:50 +0200

    Update styles, add projects

---
+++ data/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    title: "Bookstore",
+    description: "TAn interactive bookstore with React ",
+    imageUrl: "./images/project_images/bookme.png",
+    technologies: ["React", "Redux", "JavaScript", "Ruby", "Swipper"],
+    liveVersion: "Coming soon",
+    source: "https://github.com/AlyAtalla/bookstore",
+  },
+
+  {
+    title: "Catalogue-of-my-things",
+    description: "An interactive app built with Ruby to add remove,and edit Book collections, Albums, Films and more.",
+    imageUrl: "./images/project_images/foodify_website.png",
+    technologies: ["MEALDB API", "JavaScript", "Webpack", "CSS", "Html", "ES6 modules"],
+    liveVersion: "testing",
+    source: "https://github.com/AlyAtalla/Catalogue-of-my-things",
+  },
+
+  {
+    title: "Space-Travelers",
+    description: "In this project, we worked with the real live data from the SpaceX API. Our task was to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions..",
+    imageUrl: "./images/project_images/Cryptocurrency_project.webp",
+    technologies: ["React", "Redux", "Jest", "JavaScript", "CSS", "Html"],
+    liveVersion: "coming soon",
+    source: "https://github.com/AlyAtalla/Space-Travelers",
+  },
+
+  {
+    title: "budget-app",
+    description: "A classic budget app to keep track of your expenses by category, made with Rails, Turbo's Stimulus and CSS.",
+    imageUrl: "./images/project_images/budget-app.png",
+    technologies: ["Ruby on Rails", "Ruby", "Devise", "JavaScript", "CSS", "Html"],
+    liveVersion: "coming Soon",
+    source: "https://github.com/AlyAtalla/budget-app",
+  },
+
+  {
+    title: "blog-app",
+    description: "The Blog app is a classic example of a blog website. It's a functional website that shows the list of posts and empower readers to interact with them by adding comments and liking post.",
+    imageUrl: "./images/project_images/travell_website_project.jpeg",
+    technologies: ["JavaScript", "Webpack", "CSS", "Html", "ES6 modules"],
+    liveVersion: "coming soon",
+    source: "https://github.com/AlyAtalla/blog-app",
+  },
+
+  {
+    title: "Math Magicians",
+    description: "In this project I am creating a Single Page App using React regarding basic Calculator and its functionalities.",
+    imageUrl: "./images/project_images/Math-majician-project.jpeg",
+    technologies: ["React", "JavaScript", "CSS", "ES6 modules", "JSX"],
+    liveVersion: "coming soon",
+    source: "https://github.com/AlyAtalla/math-magician",
+  },
+];
+
+export default projects;
